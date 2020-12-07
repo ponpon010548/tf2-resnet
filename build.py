d9c36b032b35a617764735dab5a8fabc742e095c
Yi-Fan Peng 彭羿凡 <ponpon010548@gmail.com>
2020-12-08 05:11:53 +0800

    Add files via upload

---
+++ build.py
@@ -0,0 +1,98 @@
+import tensorflow as tf
+import tensorflow.keras.layers as layers
+from resnet import *
+
+def build_net(cfg, CLASSES, initializer = tf.keras.initializers.HeUniform()):
+    '''Build resnet.\n
+    Args:
+        cfg: string, type of net (options: 'resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152').
+        CLASSES: number of classes.
+        initializer: kernel initializers (default: HeUniform).
+
+    Return:
+        resnet model you selected.
+    '''
+    if int(tf.__version__[2]) < 3 and initializer == tf.keras.initializers.HeUniform():
+        initializer = tf.keras.initializers.he_uniform()
+    conv2_x, conv3_x, conv4_x, conv5_x = net_cfg(cfg, initializer)
+    net = Resnet(CLASSES, conv2_x, conv3_x, conv4_x, conv5_x, initializer)
+    net._name = cfg
+    return net
+
+
+def net_cfg(cfg, initializer):
+    '''Select type of resnet.\n
+    Arg:
+        cfg: type of net (options: 'resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152').
+
+    Return:
+        conv2_x, conv3_x, conv4_x, conv5_x (layer list for each stage).
+    '''
+    conv2_x = []
+    conv3_x = []
+    conv4_x = []
+    conv5_x = []
+    small = False
+    if cfg == 'resnet18':
+        layer_list = [2, 2, 2, 2]
+        small = True
+    elif cfg == 'resnet34':
+        layer_list = [3, 4, 6, 3]
+        small = True
+    elif cfg == 'resnet50':
+        layer_list = [3, 4, 6, 3]
+    elif cfg == 'resnet101':
+        layer_list = [3, 4, 23, 3]
+    elif cfg == 'resnet152':
+        layer_list = [3, 8, 36, 3]
+    else:
+        raise Exception("No such option!")
+    for i in range(layer_list[0]):
+        if i == 0:
+            if small:
+                conv2_x.append(SmallConvBlock(64, 1, "stage2_{}".format(i + 1), initializer))
+            else:
+                conv2_x.append(ConvBlock(64, 1, "stage2_{}".format(i + 1), initializer))
+        else:
+            if small:
+                conv2_x.append(SmallIdentityBlock(64, "stage2_{}".format(i + 1), initializer))
+            else:
+                conv2_x.append(IdentityBlock(64, "stage2_{}".format(i + 1), initializer))
+    #conv3_x
+    for i in range(layer_list[1]):
+        if i == 0:
+            if small:
+                conv3_x.append(SmallConvBlock(128, 2, "stage3_{}".format(i + 1), initializer))
+            else:
+                conv3_x.append(ConvBlock(128, 2, "stage3_{}".format(i + 1), initializer))
+        else:
+            if small:
+                conv3_x.append(SmallIdentityBlock(128, "stage3_{}".format(i + 1), initializer))
+            else:
+                conv3_x.append(IdentityBlock(128, "stage3_{}".format(i + 1), initializer))
+    #conv4_x
+    for i in range(layer_list[2]):
+        if i == 0:
+            if small:
+                conv4_x.append(SmallConvBlock(256, 2, "stage4_{}".format(i + 1), initializer))
+            else:
+                conv4_x.append(ConvBlock(256, 2, "stage4_{}".format(i + 1), initializer))
+        else:
+            if small:
+                conv4_x.append(SmallIdentityBlock(256, "stage4_{}".format(i + 1), initializer))
+            else:
+                conv4_x.append(IdentityBlock(256, "stage4_{}".format(i + 1), initializer))
+    #conv5_x
+    for i in range(layer_list[3]):
+        if i == 0:
+            if small:
+                conv5_x.append(SmallConvBlock(512, 2, "stage5_{}".format(i + 1), initializer))
+            else:
+                conv5_x.append(ConvBlock(512, 2, "stage5_{}".format(i + 1), initializer))
+        else:
+            if small:
+                conv5_x.append(SmallIdentityBlock(512, "stage5_{}".format(i + 1), initializer))
+            else:
+                conv5_x.append(IdentityBlock(512, "stage5_{}".format(i + 1), initializer))
+
+    return conv2_x, conv3_x, conv4_x, conv5_x
---
+++ resnet.py
@@ -0,0 +1,180 @@
+import tensorflow as tf
+import tensorflow.keras.layers as layers
+
+class Resnet(tf.keras.Model):
+    '''Resnet model.\n
+    Args:
+        CLASSES: number of classes.
+        conv2_x: conv2_x layer list.
+        conv3_x: conv3_x layer list.
+        conv4_x: conv4_x layer list.
+        conv5_x: conv5_x layer list.
+        (Note: conv2_x, conv3_x, conv4_x, conv5_x can be obtained via function net_cfg.)
+        initializer: kernel initializers (default: HeUniform).
+    
+    Return:
+        The resnet model
+    '''
+
+    def __init__(self, CLASSES, conv2_x, conv3_x, conv4_x, conv5_x, initializer = tf.keras.initializers.HeUniform()):
+        super(Resnet, self).__init__()
+        self.head_conv = layers.Conv2D(filters = 64, kernel_size = (7, 7), strides = (2, 2), padding = "same",\
+                                  kernel_initializer = initializer, name = "stage1_conv")
+        self.head_pool = layers.MaxPool2D(pool_size = (3, 3), strides = (2, 2), name = "stage2_pool")
+        self.conv2_x = tf.keras.Sequential(conv2_x, name = "stage2")
+        self.conv3_x = tf.keras.Sequential(conv3_x, name = "stage3")
+        self.conv4_x = tf.keras.Sequential(conv4_x, name = "stage4")
+        self.conv5_x = tf.keras.Sequential(conv5_x, name = "stage5")
+        self.avg_pool = layers.GlobalAveragePooling2D(name="avg_pool")
+        self.dense = layers.Dense(CLASSES, kernel_initializer = initializer, name = "dense")
+
+    def call(self, x, training = False):
+        x = self.head_conv(x)
+        x = self.head_pool(x)
+        x = self.conv2_x(x, training)
+        x = self.conv3_x(x, training)
+        x = self.conv4_x(x, training)
+        x = self.conv5_x(x, training)
+        x = self.avg_pool(x)
+        x = self.dense(x)
+        return x
+        
+class IdentityBlock(tf.keras.Model):
+    '''Identity Block of renet.\n
+    Args:
+        filters: number of filters for first layer.
+        name: name for IdentityBlock.
+        initializer: kernel_initializer (default: HeUniform).
+
+    Return:
+        The identity block.
+
+    '''
+    def __init__(self, filters, name, initializer = tf.keras.initializers.HeUniform()):
+        super(IdentityBlock, self).__init__()
+        self.conv_1 = layers.Conv2D(filters = filters, kernel_size = (1, 1), strides = (1, 1),\
+                                    padding = "same", kernel_initializer = initializer, name = name + "_conv_1")
+        self.BN_1 = layers.BatchNormalization(name = name + "_BN_1")
+        self.conv_2 = layers.Conv2D(filters = filters, kernel_size = (3, 3), strides = (1, 1),\
+                                    padding = "same", kernel_initializer = initializer, name = name + "_conv_2")
+        self.BN_2 = layers.BatchNormalization(name = name + "_BN_2")
+        self.conv_3 = layers.Conv2D(filters = filters * 4, kernel_size = (1, 1), strides = (1, 1),\
+                                    padding = "same", kernel_initializer = initializer, name = name + "_conv_3")
+        self.BN_3 = layers.BatchNormalization(name = name + "_BN_3")
+
+    def call(self, inputs, training = False):
+        x = self.conv_1(inputs)
+        x = self.BN_1(x, training = training)
+        x = tf.keras.activations.relu(x)
+        x = self.conv_2(x)
+        x = self.BN_2(x, training = training)
+        x = tf.keras.activations.relu(x)
+        x = self.conv_3(x)
+        x = self.BN_3(x, training = training)
+        x += inputs
+        return tf.keras.activations.relu(x)
+
+class ConvBlock(tf.keras.Model):
+    '''Conv Block of renet.\n
+    Args:
+        filters: number of filters for first layer.
+        stride: stride for first conv layer and short cut.
+        name: name for convBlock.
+        initializer: kernel_initializer (default: HeUniform).
+
+    Return:
+        The conv block.
+
+    '''
+    def __init__(self, filters, stride, name, initializer = tf.keras.initializers.HeUniform()):
+        super(ConvBlock, self).__init__()
+        self.conv_1 = layers.Conv2D(filters = filters, kernel_size = (1, 1), strides = (stride, stride),\
+                                    padding = "same", kernel_initializer = initializer, name = name + "_conv_1")
+        self.BN_1 = layers.BatchNormalization(name = name + "_BN_1")
+        self.conv_2 = layers.Conv2D(filters = filters, kernel_size = (3, 3), strides = (1, 1),\
+                                    padding = "same", kernel_initializer = initializer, name = name + "_conv_2")
+        self.BN_2 = layers.BatchNormalization(name = name + "_BN_2")
+        self.conv_3 = layers.Conv2D(filters = filters * 4, kernel_size = (1, 1), strides = (1, 1),\
+                                    padding = "same", kernel_initializer = initializer, name = name + "_conv_3")
+        self.BN_3 = layers.BatchNormalization(name = name + "_BN_3")
+        self.short_cut = layers.Conv2D(filters = filters * 4, kernel_size = (1, 1), strides = (stride, stride),\
+                                    padding = "same", kernel_initializer = initializer, name = name + "_conv_short")
+        self.BN_short = layers.BatchNormalization(name = name + "_BN_short")
+    
+    def call(self, inputs, training = False):
+        x = self.conv_1(inputs)
+        x = self.BN_1(x, training = training)
+        x = tf.keras.activations.relu(x)
+        x = self.conv_2(x)
+        x = self.BN_2(x, training = training)
+        x = tf.keras.activations.relu(x)
+        x = self.conv_3(x)
+        x = self.BN_3(x, training = training)
+        short = self.short_cut(inputs)
+        short = self.BN_short(short, training = training)
+        x += short
+        return tf.keras.activations.relu(x)
+
+class SmallIdentityBlock(tf.keras.Model):
+    '''Identity Block of renet18, 34.\n
+    Args:
+        filters: number of filters for first layer.
+        name: name for IdentityBlock.
+        initializer: kernel_initializer (default: HeUniform).
+
+    Return:
+        The identity block.
+
+    '''
+    def __init__(self, filters, name, initializer = tf.keras.initializers.HeUniform()):
+        super(SmallIdentityBlock, self).__init__()
+        self.conv_1 = layers.Conv2D(filters = filters, kernel_size = (3, 3), strides = (1, 1),\
+                                    padding = "same", kernel_initializer = initializer, name = name + "_conv_1")
+        self.BN_1 = layers.BatchNormalization(name = name + "_BN_1")
+        self.conv_2 = layers.Conv2D(filters = filters, kernel_size = (3, 3), strides = (1, 1),\
+                                    padding = "same", kernel_initializer = initializer, name = name + "_conv_2")
+        self.BN_2 = layers.BatchNormalization(name = name + "_BN_2")
+
+    def call(self, inputs, training = False):
+        x = self.conv_1(inputs)
+        x = self.BN_1(x, training = training)
+        x = tf.keras.activations.relu(x)
+        x = self.conv_2(x)
+        x = self.BN_2(x, training = training)
+        x += inputs
+        return tf.keras.activations.relu(x)
+
+class SmallConvBlock(tf.keras.Model):
+    '''Conv Block of renet18, 34.\n
+    Args:
+        filters: number of filters for first layer.
+        stride: stride for first conv layer and short cut.
+        name: name for convBlock.
+        initializer: kernel_initializer (default: HeUniform).
+
+    Return:
+        The conv block.
+
+    '''
+    def __init__(self, filters, stride, name, initializer = tf.keras.initializers.HeUniform()):
+        super(SmallConvBlock, self).__init__()
+        self.conv_1 = layers.Conv2D(filters = filters, kernel_size = (3, 3), strides = (stride, stride),\
+                                    padding = "same", kernel_initializer = initializer, name = name + "_conv_1")
+        self.BN_1 = layers.BatchNormalization(name = name + "_BN_1")
+        self.conv_2 = layers.Conv2D(filters = filters, kernel_size = (3, 3), strides = (1, 1),\
+                                    padding = "same", kernel_initializer = initializer, name = name + "_conv_2")
+        self.BN_2 = layers.BatchNormalization(name = name + "_BN_2")
+        self.short_cut = layers.Conv2D(filters = filters, kernel_size = (3, 3), strides = (stride, stride),\
+                                    padding = "same", kernel_initializer = initializer, name = name + "_conv_short")
+        self.BN_short = layers.BatchNormalization(name = name + "_BN_short")
+    
+    def call(self, inputs, training = False):
+        x = self.conv_1(inputs)
+        x = self.BN_1(x, training = training)
+        x = tf.keras.activations.relu(x)
+        x = self.conv_2(x)
+        x = self.BN_2(x, training = training)
+        short = self.short_cut(inputs)
+        short = self.BN_short(short, training = training)
+        x += short
+        return tf.keras.activations.relu(x)
